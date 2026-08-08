3664d213c01bbcf6a989b6d882d04a034a4c1dfd
MALAYA RANJAN PRADHAN <166914295+dev0malaya@users.noreply.github.com>
2026-08-09 01:28:08 +0530

    Create terraformer.sh

---
+++ terraformer.sh
@@ -0,0 +1,11 @@
+sudo yum update -y
+
+sudo yum install -y wget unzip
+
+wget https://github.com/GoogleCloudPlatform/terraformer/releases/download/0.8.24/terraformer-aws-linux-amd64 -O terraformer
+
+chmod +x terraformer
+sudo mv terraformer /usr/local/bin/
+
+echo 'export PATH=$PATH:/usr/local/bin' >> ~/.bashrc
+source ~/.bashrc
